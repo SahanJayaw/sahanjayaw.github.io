73f7776825bd453b6c05f3d8da3ced68431d8f80
SahanJayaw <onlyinlanka119@gmail.com>
2026-01-25 00:19:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+
+  document.querySelectorAll(".hidden").forEach(el => observer.observe(el));
+});
 
